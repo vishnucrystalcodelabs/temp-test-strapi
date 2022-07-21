862e8d4d832a2ebc73f800f23bbbcd08d7ac902b
crystalcodelabs <mahavishnu@crystalcodelabs.com>
2022-07-21 11:48:24 +0530

    remove strapi-temp-project

--- strapi-master/.github/actions/security/lockfile/script.sh
+++
@@ -1,7 +0,0 @@
-yarn global add lockfile-lint
-
-lockfile-lint \
-    --type $LOCKFILE_TYPE \
-    --path $LOCKFILE_PATH \
-    --allowed-hosts $LOCKFILE_ALLOWED_HOSTS \
-    --validate-https
--- strapi-master/jest.config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = {
-  projects: ['<rootDir>/packages/**/jest.config.js', '<rootDir>/.github'],
-};
